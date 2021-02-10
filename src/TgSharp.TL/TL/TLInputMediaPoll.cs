bb2f1e337f041e816a8331f4364b54bc39891c6f
مُعَاذ القَرْنِي <60256639+Muaath5@users.noreply.github.com>
2021-02-10 09:00:20 +0300

    Update to layer 121

--- src/TgSharp.TL/TL/TLInputMediaPoll.cs
+++ src/TgSharp.TL/TL/TLInputMediaPoll.cs
@@ -20,7 +20,11 @@ namespace TgSharp.TL
             }
         }
 
+        public int Flags { get; set; }
         public TLPoll Poll { get; set; }
+        public TLVector<byte> CorrectAnswers { get; set; }
+        public string Solution { get; set; }
+        public TLVector<TLMessageEntity> SolutionEntities { get; set; }
 
         public void ComputeFlags()
         {
@@ -29,13 +33,36 @@ namespace TgSharp.TL
 
         public override void DeserializeBody(BinaryReader br)
         {
+            Flags = br.ReadInt32();
             Poll = (TLPoll)ObjectUtils.DeserializeObject(br);
+            if (Flags & 1 != 0)
+                CorrectAnswers = (TLVector<byte>)ObjectUtils.DeserializeVector<byte>(br);
+            else
+                CorrectAnswers = null;
+            if (Flags & 2 != 0)
+            {
+                Solution = StringUtil.Deserialize(br);
+                SolutionEntities = (TLVector<TLMessageEntity>)ObjectUtils.DeserializeVector<TLMessageEntity>(br);
+            }
+            else
+            {
+                Solution = null;
+                SolutionEntities = null;
+            }
         }
 
         public override void SerializeBody(BinaryWriter bw)
         {
             bw.Write(Constructor);
+            bw.Write(Flags);
             ObjectUtils.SerializeObject(Poll, bw);
+            if (Flags & 1 != 0)
+                ObjectUtils.SerializeObject(CorrectAnswers, bw);
+            if (Flags & 2 != 0)
+            {
+                StringUtil.Serialize(Solution, bw);
+                ObjectUtils.SerializeObject(SolutionEntities, bw);
+            }
         }
     }
 }
